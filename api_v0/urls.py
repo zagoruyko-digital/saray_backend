49c280ff3f1d3fbe8a5eba96598140fc51d9b3fc
Viktor Zagoruyko <zagoruyko.digital@gmail.com>
2018-11-21 00:58:58 +0500

    User Info API

--- api_v0/urls.py
+++ api_v0/urls.py
@@ -4,6 +4,7 @@ from .views import *
 
 router = DefaultRouter()
 router.register(r'news', NewsViewSet)
+router.register(r'user', UserViewSet, base_name='CurrentUser')
 
 
 urlpatterns = router.urls
--- api_v0/views.py
+++ api_v0/views.py
@@ -1,7 +1,17 @@
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework import viewsets
+from rest_framework.authtoken.models import Token
 from rest_framework.decorators import permission_classes
 from rest_framework.permissions import AllowAny
+from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_200_OK
+from rest_framework.response import Response
 from .serializers import *
+
+@csrf_exempt
+def user(request):
+   current_user = User.objects.get(id=1)
+   
+   UserViewSet.get_serializer_class
 
 @permission_classes((AllowAny,))
 class NewsViewSet(viewsets.ReadOnlyModelViewSet):
@@ -11,3 +21,9 @@ class NewsViewSet(viewsets.ReadOnlyModelViewSet):
         if self.action == 'list':
            return NewsDetailSerializer
         return NewsDetailSerializer
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+   serializer_class = ManagerInfoSerializer
+
+   def get_queryset(self):
+      return User.objects.filter(id=self.request.user.id)
